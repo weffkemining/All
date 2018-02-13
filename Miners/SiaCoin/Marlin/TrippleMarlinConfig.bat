59c231126fcc6d6315882c7b338e6b4f39390334
Weffke <weffke@gmail.com>
2018-02-13 22:37:01 +0100

    lower settings

--- Miners/SiaCoin/Marlin/TrippleMarlinConfig.bat
+++ Miners/SiaCoin/Marlin/TrippleMarlinConfig.bat
@@ -15,12 +15,12 @@ SET /P pool= [Mining pool server (URL:port - Example eu.luxor.tech:3333)]
 
 @echo :: Script generated with configbuilder by weffke > sia-mid.bat
 @echo :: Visit www.weffkemining.com >> sia-mid.bat
-@echo marlin.exe --user %address% --intensity 20 --host %pool% >> sia-mid.bat
+@echo marlin.exe --user %address% --intensity 18 --host %pool% >> sia-mid.bat
 
 
 @echo :: Script generated with configbuilder by weffke > sia-low.bat
 @echo :: Visit www.weffkemining.com >> sia-low.bat
-@echo marlin.exe --user %address% --intensity 18 --host %pool% >> sia-low.bat
+@echo marlin.exe --user %address% --intensity 14 --host %pool% >> sia-low.bat
 
 ECHO Files have been created
 pause
